a2fe505a51b7a79aebf3b1ffe2ad840e6d284ac0
Martin Devlin <martin.devlin@pearson.com>
2019-03-25 10:21:57 +0000

    Add EXPORT_AS

--- README.md
+++ README.md
@@ -13,10 +13,17 @@ CONSUL_KEY Key to wait for. Optional.
 
 EXPORT_KEY Boolean to determine whether or not to write value to filesystem. Optional.
 
+EXPORT_AS Export as env var, not as file. Optional.
+
 ## Notes
 
-EXPORT_KEY if true will write out the value of CONSUL_KEY to /pod-data/consul/${CONSUL_KEY}.
-
+EXPORT_KEY if true will eith write out the value of CONSUL_KEY to /pod-data/consul/${CONSUL_KEY},
 This is intended to allow sharing of values with other containers in the pod via a VolumeMount (/pod-data/consul).
+
+Alternatively, if EXPORT_AS is not empty, will write to /pod-data/exports/consul.sh:
+
+`"export ${EXPORT_AS}=${CONSUL_VAL}"`
+
+/pod-data/exports/consul.sh can be sourced by other containers' entrypoints to inherit these values.
 
 ## End
--- run.sh
+++ run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# comma-delimited list of host:port,host:port
 if [ "x${CONSUL_HTTP_ADDR}x" == "xx" ]; then
     echo "Set CONSUL_HTTP_ADDR"
     exit 0
 fi
 
 rc=`curl -k -s -o /dev/null -w "%{http_code}" ${CONSUL_HTTP_ADDR}/v1/agent/self?token=${CONSUL_HTTP_TOKEN}`
+resp="false"
 
-if [ $rc == "200" ]; then
-    echo "Consul alive at ${CONSUL_HTTP_ADDR}"
-elif [ $rc == "403" ]; then
-    echo "Check CONSUL_HTTP_TOKEN: received 403 from ${CONSUL_HTTP_ADDR}/v1/agent/self"
-    exit 1
-else
-    echo "Unexpected response code ${rc} from ${CONSUL_HTTP_ADDR}/v1/agent/self"
-fi
+while [ $resp == "false" ]; do
+    if [ $rc == "200" ]; then
+        echo "Consul alive at ${CONSUL_HTTP_ADDR}"
+        resp="true"
+    elif [ $rc == "403" ]; then
+        echo "Check CONSUL_HTTP_TOKEN: received 403 from ${CONSUL_HTTP_ADDR}/v1/agent/self"
+        resp="true"
+        exit 1
+    else
+        echo "Unexpected response code ${rc} from ${CONSUL_HTTP_ADDR}/v1/agent/self"
+        sleep 10
+        rc=`curl -k -s -o /dev/null -w "%{http_code}" ${CONSUL_HTTP_ADDR}/v1/agent/self?token=${CONSUL_HTTP_TOKEN}`
+    fi
+done
 
 if [ "x${CONSUL_KEY}x" == "xx" ]; then
     echo "No CONSUL_KEY specified, Exiting."
@@ -37,11 +43,16 @@ fi
 
 CONSUL_VAL=`curl -k -s $CONSUL_HTTP_ADDR/v1/kv/${CONSUL_KEY}?token=${CONSUL_HTTP_TOKEN} | jq -r .[].Value | base64 -d`
 
-echo "Exporting /pod-data/consul/${CONSUL_KEY}."
+mkdir -p /pod-data/consul  /pod-data/exports
 
-mkdir -p `dirname /pod-data/consul/${CONSUL_KEY}`
-
-echo ${CONSUL_VAL} > /pod-data/consul/${CONSUL_KEY}
+if [ "x${EXPORT_AS}x" == "xx" ]; then
+  echo "Exporting /pod-data/consul/${CONSUL_KEY}."
+  echo ${CONSUL_VAL} > /pod-data/consul/${CONSUL_KEY}
+else
+  echo "Exporting ${CONSUL_KEY} as var."
+  echo "export ${EXPORT_AS}=${CONSUL_VAL}" >> /pod-data/exports/consul.sh
+  chmod 750 /pod-data/exports/consul.sh
+fi
 
 echo "End"
 exit 0
